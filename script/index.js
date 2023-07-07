b1395bef8c10cd4ed8d7517586363df6f58b8399
Geykin Mikhail <mgeykin@bk.ru>
2023-07-07 17:41:43 +0300

    fix html

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-// Получаем необходимые элементы
+
 const editBtn = document.querySelector('.profile__edit-btn');
 const popup = document.querySelector('.popup');
 const popupNameInput = document.querySelector('.popup__input_type_name');
@@ -8,29 +8,24 @@ const closeBtn = document.querySelector('.popup__close');
 const profileName = document.querySelector('.profile__title');
 const profileAbout = document.querySelector('.profile__subtitle');
 
-// Функция открытия попапа
 function openPopup() {
   popup.classList.add('popup_opened');
   popupNameInput.value = profileName.textContent;
   popupAboutInput.value = profileAbout.textContent;
 }
 
-// Функция закрытия попапа
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
 
-// Функция сохранения данных
 function saveData(event) {
-  event.preventDefault(); // Предотвращаем отправку формы (перезагрузку страницы)
-
+  event.preventDefault(); 
   profileName.textContent = popupNameInput.value;
   profileAbout.textContent = popupAboutInput.value;
 
   closePopup();
 }
 
-// Назначаем обработчики событий
 editBtn.addEventListener('click', openPopup);
 closeBtn.addEventListener('click', closePopup);
 saveBtn.addEventListener('click', saveData);
